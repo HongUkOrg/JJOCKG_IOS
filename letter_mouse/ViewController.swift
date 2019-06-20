c9034cf61fc7b6f107532ba82324cbaa9936822e
박홍욱 <david.park@igaworks.com>
2019-06-20 18:25:36 +0900

    keyboard 지원, 1.2 배포

--- letter_mouse/ViewController.swift
+++ letter_mouse/ViewController.swift
@@ -15,34 +15,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("view did loaded on main")
          self.navigationController?.isNavigationBarHidden = true
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.handleGesture(gesture:)))
-        swipeLeft.direction = .left
-        self.view.addGestureRecognizer(swipeLeft)
         
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.handleGesture(gesture:)))
-        swipeRight.direction = .right
-        self.view.addGestureRecognizer(swipeRight)
-        // Do any additional setup after loading the view, typically from a nib.
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1000) ) {
+            self.performSegue(withIdentifier: "toCartoon01", sender: self)
+        }
     }
     @IBOutlet var splashView: UIView!
     
-    
-    @objc func handleGesture(gesture: UISwipeGestureRecognizer) -> Void {
-        if gesture.direction == .right {
-            print("Swipe Right")
-            
-        }
-        else if gesture.direction == .left {
-            performSegue(withIdentifier: "toCartoon01", sender: self)
-            print("Swipe Left")
-           
-            
-        }
-
-    
-
-        weak var mainLetter: UILabel!
-    
-}
 
 }
